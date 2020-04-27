312aff8aa667b62dfca3344ed7446fa5efa2330f
haris sucipto <harisssucipto@gmail.com>
2020-04-27 08:25:23 +0700

    wkkww

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use("/api/v1", router);
 
 app.listen(port, () =>
-  console.log(`Server b is running in port a ${port}`)
+  console.log(`Server c is running in port a ${port}`)
 );
